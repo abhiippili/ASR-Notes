734e6a67265846097b7210c91d7c2e37ef260a6c
unknown <abhiippili1234@gmail.com>
2023-05-03 01:08:40 +0530

    registration page

--- client/src/Pages/Register.js
+++ client/src/Pages/Register.js
@@ -1,20 +1,37 @@
 import {
   Box,
   Button,
+  IconButton,
   InputAdornment,
   MenuItem,
   Paper,
   styled,
-  TextField
+  TextField,
+  Typography
 } from "@mui/material";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import EmailIcon from "@mui/icons-material/Email";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import CakeIcon from "@mui/icons-material/Cake";
+import MaleIcon from "@mui/icons-material/Male";
+import PlaceIcon from "@mui/icons-material/Place";
+import ClassIcon from "@mui/icons-material/Class";
+import SchoolIcon from "@mui/icons-material/School";
+import PasswordIcon from "@mui/icons-material/Password";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+
+const CustomStack = styled(Box)({
+  display: "flex",
+  justifyContent: "flex-end",
+  padding: "0rem 8%"
+});
 
 const StyledPaper = styled(Paper)({
-  margin: "20px auto",
+  margin: "8px",
   width: 400,
-  padding: "20px 20px",
+  padding: "12px 12px",
   borderRadius: "1rem",
   background: "#ffff"
 });
@@ -49,6 +66,11 @@ const Register = () => {
 
   const navigate = useNavigate();
 
+  const [showPassword, setShowPassword] = useState(false);
+  const handleShowPassword = () => {
+    setShowPassword((showPassword) => !showPassword);
+  };
+
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setUser({ ...user, [name]: value });
@@ -56,130 +78,187 @@ const Register = () => {
   };
   return (
     <div>
-      <StyledPaper elevation={8}>
-        <Stack component="form" onSubmit={() => navigate("/")}>
-          {/* email */}
-          <FlexBox>
-            <EmailIcon />
-            <TextField
-              sx={{ marginBottom: "1rem", marginLeft: "0.5rem" }}
-              label="Email"
-              name="email"
-              fullWidth
-              required
-              type="email"
-              variant="standard"
-              color="secondary"
-              value={user.email}
-              onChange={handleInputChange}
-            />
-          </FlexBox>
-          {/* username */}
-          <TextField
-            sx={{ marginBottom: "1rem" }}
-            label="UserName"
-            name="username"
-            fullWidth
-            required
-            type="text"
-            variant="standard"
-            color="secondary"
-            value={user.username}
-            onChange={handleInputChange}
-          />
-          {/* date of birth */}
-          <TextField
-            sx={{ marginBottom: "1rem" }}
-            label="Date Of Birth"
-            name="dateOfBirth"
-            fullWidth
-            required
-            type="date"
-            variant="standard"
-            color="secondary"
-            value={user.dateOfBirth}
-            onChange={handleInputChange}
-          />
-          {/* gender */}
-          <TextField
-            select
-            name="gender"
-            label="Gender"
-            value={user.gender}
-            variant="standard"
-            color="secondary"
-            onChange={handleInputChange}
-          >
-            {genders.map((gender) => (
-              <MenuItem value={gender}>{gender}</MenuItem>
-            ))}
-          </TextField>
+      <CustomStack>
+        <StyledPaper elevation={8}>
+          <Stack component="form" onSubmit={() => navigate("/")}>
+            <Typography
+              variant="h6"
+              fontFamily="Poppins"
+              align="center"
+              fontWeight={600}
+            >
+              Register as a student
+            </Typography>
+            {/* email */}
+            <FlexBox>
+              <EmailIcon />
+              <TextField
+                sx={{ marginBottom: "1rem", marginLeft: "0.5rem" }}
+                label="Email"
+                name="email"
+                fullWidth
+                required
+                type="email"
+                variant="standard"
+                color="secondary"
+                value={user.email}
+                onChange={handleInputChange}
+              />
+            </FlexBox>
+            {/* username */}
+            <FlexBox>
+              <AccountCircleIcon />
+              <TextField
+                sx={{ marginBottom: "1rem", marginLeft: "0.5rem" }}
+                label="UserName"
+                name="username"
+                fullWidth
+                required
+                type="text"
+                variant="standard"
+                color="secondary"
+                value={user.username}
+                onChange={handleInputChange}
+              />
+            </FlexBox>
+            {/* date of birth */}
+            <FlexBox>
+              <CakeIcon />
+              <TextField
+                sx={{ marginBottom: "1rem", marginLeft: "0.5rem" }}
+                label="Date Of Birth"
+                name="dateOfBirth"
+                fullWidth
+                required
+                type="date"
+                variant="standard"
+                color="secondary"
+                value={user.dateOfBirth}
+                onChange={handleInputChange}
+              />
+            </FlexBox>
+            {/* gender */}
 
-          {/* city */}
-          <TextField
-            sx={{ marginBottom: "1rem", marginTop: "0.8rem" }}
-            label="City"
-            name="city"
-            fullWidth
-            type="text"
-            variant="standard"
-            color="secondary"
-            value={user.city}
-            onChange={handleInputChange}
-          />
-          {/* course */}
-          <TextField
-            sx={{ marginBottom: "1rem" }}
-            label="Course"
-            name="course"
-            fullWidth
-            type="text"
-            variant="standard"
-            color="secondary"
-            value={user.course}
-            onChange={handleInputChange}
-          />
-          {/* college */}
-          <TextField
-            sx={{ marginBottom: "1rem" }}
-            label="College"
-            name="college"
-            fullWidth
-            type="text"
-            variant="standard"
-            color="secondary"
-            value={user.college}
-            onChange={handleInputChange}
-          />
-          {/* password */}
-          <TextField
-            sx={{ marginBottom: "1rem" }}
-            fullWidth
-            required
-            label="Password"
-            name="password"
-            color="secondary"
-            type="password"
-            variant="standard"
-            onChange={handleInputChange}
-          />
-          {/* confirm password */}
-          <TextField
-            sx={{ marginBottom: "1rem" }}
-            fullWidth
-            required
-            label="Confirm Password"
-            name="confirmPassword"
-            color="secondary"
-            type="password"
-            variant="standard"
-            onChange={handleInputChange}
-          />
-          <Button variant="contained" color="secondary" type="submit">
-            Register
-          </Button>
-        </Stack>
-      </StyledPaper>
+            <FlexBox sx={{ alignItems: "flex-end" }}>
+              <MaleIcon />
+              <TextField
+                select
+                fullWidth
+                sx={{ marginLeft: "0.5rem" }}
+                name="gender"
+                label="Gender"
+                value={user.gender}
+                variant="standard"
+                color="secondary"
+                onChange={handleInputChange}
+              >
+                {genders.map((gender) => (
+                  <MenuItem value={gender}>{gender}</MenuItem>
+                ))}
+              </TextField>
+            </FlexBox>
+
+            {/* city */}
+            <FlexBox sx={{ marginTop: "0.8rem" }}>
+              <PlaceIcon />
+              <TextField
+                sx={{
+                  marginBottom: "1rem",
+                  marginLeft: "0.5rem"
+                }}
+                label="City"
+                name="city"
+                fullWidth
+                type="text"
+                variant="standard"
+                color="secondary"
+                value={user.city}
+                onChange={handleInputChange}
+              />
+            </FlexBox>
+            {/* course */}
+            <FlexBox>
+              <ClassIcon />
+              <TextField
+                sx={{ marginBottom: "1rem", marginLeft: "0.5rem" }}
+                label="Course"
+                name="course"
+                fullWidth
+                type="text"
+                variant="standard"
+                color="secondary"
+                value={user.course}
+                onChange={handleInputChange}
+              />
+            </FlexBox>
+            {/* college */}
+            <FlexBox>
+              <SchoolIcon />
+              <TextField
+                sx={{ marginBottom: "1rem", marginLeft: "0.5rem" }}
+                label="College (Registered Id)"
+                name="college"
+                fullWidth
+                required
+                type="text"
+                variant="standard"
+                color="secondary"
+                value={user.college}
+                onChange={handleInputChange}
+              />
+            </FlexBox>
+            {/* password */}
+            <FlexBox>
+              <PasswordIcon />
+              <TextField
+                sx={{ marginBottom: "1rem", marginLeft: "0.5rem" }}
+                fullWidth
+                required
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        onClick={handleShowPassword}
+                        onMouseDown={(e) => e.preventDefault()}
+                      >
+                        {showPassword ? (
+                          <VisibilityOffIcon />
+                        ) : (
+                          <VisibilityIcon />
+                        )}
+                      </IconButton>
+                    </InputAdornment>
+                  )
+                }}
+                label="Password"
+                name="password"
+                color="secondary"
+                type={showPassword ? "text" : "password"}
+                variant="standard"
+                onChange={handleInputChange}
+              />
+            </FlexBox>
+            {/* confirm password */}
+            <FlexBox>
+              <PasswordIcon />
+              <TextField
+                sx={{ marginBottom: "1rem", marginLeft: "0.5rem" }}
+                fullWidth
+                required
+                label="Confirm Password"
+                name="confirmPassword"
+                color="secondary"
+                type="password"
+                variant="standard"
+                onChange={handleInputChange}
+              />
+            </FlexBox>
+            <Button variant="contained" color="secondary" type="submit">
+              Register
+            </Button>
+          </Stack>
+        </StyledPaper>
+      </CustomStack>
     </div>
   );
 };
